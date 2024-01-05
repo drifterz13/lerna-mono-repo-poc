79b7a11b6a4698f9c7b8b29f0746539ae24054d7
drifterz13 <totsawat@skilllane.com>
2024-01-05 17:39:10 +0700

    add acl method

--- packages/acl/src/index.ts
+++ packages/acl/src/index.ts
@@ -1,3 +1,7 @@
 export function canAccessCurriculumPanel(role: "teacher" | "student") {
   return role === "teacher";
 }
+
+export function canAccessSelfCurriculumPanel(role: "teacher" | "student") {
+  return true;
+}
