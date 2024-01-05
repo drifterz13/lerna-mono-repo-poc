4f5598675cf38d639adb18f316c1532d90c2c4be
drifterz13 <totsawat@skilllane.com>
2024-01-05 13:40:47 +0700

    update pkg name

--- apps/school-web/src/main.tsx
+++ apps/school-web/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-import { canAccessCurriculumPanel } from "acl";
+import { canAccessCurriculumPanel } from "@mixz/acl";
 
 console.log("canAccessCurriculumPanel", canAccessCurriculumPanel("student"));
 
